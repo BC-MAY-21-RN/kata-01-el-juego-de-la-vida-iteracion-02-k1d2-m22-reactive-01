9cb008321577a512ea104e7b1cfbfe68b985875a
samuel wuitron mendoza <cwuitron@ucol.mx>
2022-05-04 18:28:26 -0500

    fifth Commit, Co-authored-by: LizyMiGo <jmiguel@ucol.mx> Co-authored-by: daniel-yocupicio <dag_98@hotmail.com>

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 class Cell {
-  constructor(status, vecinos) {
+  constructor(status = ".") {
     this._status = status; //muerto ., viva *
-    this._vecinos = vecinos;
   }
   getState() {
     return this._status;
@@ -9,39 +8,21 @@ class Cell {
   setState(newState) {
     this._status = newState;
   }
-
-  escuchar_estado() {
-    if (this._vecinos < 2 && this._status == "*") {
-      this._status = ".";
-    } else if (this._vecinos > 3 && this._status == "*") {
-      this._status = ".";
-    } else if ((this._vecinos == 3 || this._vecinos == 2) && this._status == "*") {
-      this._status = "*";
-    } else if (this._vecinos == 3 && this._status == ".") {
-      this._status = "*";
-    }else {
-        console.log("El parametro obtenido no pertenece a ningun caso ");
-    }
-
-    console.log("prueba");
-    return this._status;
-  }
 }
-
 class Table {
   constructor(x, y, cadena) {
-    this.matriz = this.rellenarMatriz(x,y,cadena);
+    this.matriz = this.rellenarMatriz(x, y, cadena);
   }
 
-  generarMatriz(x,y){
-    return Array(x).fill(".").map(item => (new Array(y).fill("."))); 
+  generarMatriz(x, y) {
+    return Array(x).fill(new Cell()).map(item => (new Array(y).fill(new Cell())));
   }
 
-  rellenarMatriz(x,y,cadena){
-    let matriz = this.generarMatriz(x,y);
+  rellenarMatriz(x, y, cadena) {
+    let matriz = this.generarMatriz(x, y);
     matriz.forEach(async (fila, indexi) => {
       fila.forEach(async (casilla, indexj) => {
-        matriz[indexi][indexj] = cadena.charAt(this.randomCaracter(0, cadena.length - 1));
+        matriz[indexi][indexj] = new Cell(cadena.charAt(this.randomCaracter(0, cadena.length - 1)))
       })
     })
     return matriz;
@@ -50,10 +31,114 @@ class Table {
   randomCaracter(min, max) {
     return Math.random() * (max - min) + min;
   }
+
+  cicloVida() {
+    return "";
+  }
+
+  /*
+  ["-"],["-"],["-"],["-"]
+  ["-"],["*"],["-"],["-"]
+  ["-"],["*"],["-"],["-"]
+  ["-"],["-"],["-"],["-"]
+  */
+
+  vecinosVivos(x,y) {
+    var vecinosVivos = 0;
+    const objeto = {
+      esquinaSuperiorIzquierda: (fila, columna) => {
+        if (fila != 0) {
+          if (this.matriz[fila - 1][columna - 1].getState() == "*") {
+            vecinosVivos++;
+          }
+        }
+      },
+      superior: (fila, columna) => {
+        if (fila != 0) {
+          if (this.matriz[fila - 1][columna].getState() == "*") {
+            vecinosVivos++;
+          }
+        }
+      },
+      esquinaSuperiorDerecha: (fila, columna) => {
+        if (fila != 0) {
+          if (this.matriz[fila - 1][columna + 1].getState() == "*") {
+            vecinosVivos++;
+          }
+        }
+      },
+      esquinaInferiorIzquierda: (fila, columna) => {
+        if (fila != this.matriz.length - 1) {
+          if (this.matriz[fila + 1][columna - 1].getState() == "*") {
+            vecinosVivos++;
+          }
+        }
+      },
+      inferior: (fila, columna) => {
+        if (fila != this.matriz.length - 1) {
+          if (this.matriz[fila + 1][columna].getState() == "*") {
+            vecinosVivos++;
+          }
+        }
+      },
+      esquinaInferiorDerecha: (fila, columna) => {
+        if (fila != this.matriz.length - 1) {
+          if (this.matriz[fila + 1][columna + 1].getState() == "*") {
+            vecinosVivos++;
+          }
+        }
+      },
+      derecha: (fila, columna) => {
+        if (this.matriz[fila][columna + 1].getState() == "*") {
+          vecinosVivos++;
+        }
+      },
+      izquierda: (fila, columna) => {
+        if (this.matriz[fila][columna - 1].getState() == "*") {
+          vecinosVivos++;
+        }
+      },
+    }
+    Object.keys(objeto).forEach(key => objeto[key](x, y) )
+    return vecinosVivos;
+  }
+
+  imprimirMatriz(){
+
+  }
+
 }
 
-var tabla = new Table(10, 10, ".......*....***.");
-var celula = new Cell("*",3);
+var tabla = new Table(10, 10, ".......*....**....**.");
+console.log(tabla.vecinosVivos(3,3));
 
-console.log(tabla.matriz);
-console.log(celula.escuchar_estado());
+/*function counter (tablero, x, y) {
+  // Movimiento hacia los vecinos
+  let moveX = new Array(0, 0, 1, -1, -1, -1, 1, 1);
+  let moveY = new Array(1, -1, 0, 0, 1, -1, -1, 1);
+  let cnt = 0;
+  for (let cell = 0; cell < 8; cell++) {
+    let mox = x + moveX[cell];
+    let moy = y + moveY[cell];
+    if (tablero[mox][moy] == '*') {
+      cnt += 1;
+    }
+  }
+  return cnt;
+}
+
+isAliveOrDead() {
+  for (let i= 1; i < this.matriz.length() - 1; i++){
+    for(let j = 1; j <= this.matriz.length() -1; j++){
+        vivas = counter(array,i,j);
+        if (array[i][j] == '*' && vivas < 2) {
+            this.matriz[i][j] = '.';
+        } else if (array[i][j] == '*' && vivas > 3) {
+            this.matriz[i][j] = '.';
+        } else if (array[i][j] == '.' && vivas == 3) {
+            this.matriz[i][j] = '*';
+        }
+    }
+}
+return this.matriz;
+}*/
