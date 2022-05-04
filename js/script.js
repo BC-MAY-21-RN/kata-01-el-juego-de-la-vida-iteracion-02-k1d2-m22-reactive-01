91b367f8936a222525f6bf0a6fcf52c763505597
samuel wuitron mendoza <cwuitron@ucol.mx>
2022-05-04 16:53:26 -0500

    Third Commit, Co-authored-by: LizyMiGo <jmiguel@ucol.mx> Co-authored-by: daniel-yocupicio <dag_98@hotmail.com>

--- js/script.js
+++ js/script.js
@@ -1,15 +1,33 @@
-class Cell{
-  constructor(status,x,y){
-    this._status=status;
-    this._x=x;
-    this._y=y;
+class Cell {
+  constructor(status, x, y) {
+    this._status = status;
+    this._x = x;
+    this._y = y;
   }
 }
-class Table{
-  constructor(x, y){
-    this.matriz = new Array(x).fill(".").map(item => (new Array(y).fill(".")));
+class Table {
+  constructor(x, y, cadena) {
+    this.matriz = this.rellenarMatriz(x,y,cadena);
+  }
+
+  generarMatriz(x,y){
+    return Array(x).fill(".").map(item => (new Array(y).fill("."))); 
+  }
+
+  rellenarMatriz(x,y,cadena){
+    let matriz = this.generarMatriz(x,y);
+    matriz.forEach(async (fila, indexi) => {
+      fila.forEach(async (casilla, indexj) => {
+        matriz[indexi][indexj] = cadena.charAt(this.randomCaracter(0, cadena.length - 1));
+      })
+    })
+    return matriz;
+  }
+
+  randomCaracter(min, max) {
+    return Math.random() * (max - min) + min;
   }
 }
 
-var tabla = new Table(10,10);
+var tabla = new Table(10, 10, ".......*....***.");
 console.log(tabla.matriz);
