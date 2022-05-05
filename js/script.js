272e74295dab05b21e2a86ab59af7e2e41e09457
samuel wuitron mendoza <cwuitron@ucol.mx>
2022-05-04 19:15:24 -0500

    game of life finished, Co-authored-by: LizyMiGo <jmiguel@ucol.mx> Co-authored-by: daniel-yocupicio <dag_98@hotmail.com>

--- js/script.js
+++ js/script.js
@@ -1,40 +1,40 @@
 class Cell {
   constructor(status = ".") {
-    this._status = status; //muerto ., viva *
+      this.status = status; //muerto ., viva *
   }
   getState() {
-    return this._status;
+      return this.status;
   }
   setState(newState) {
-    this._status = newState;
+      this.status = newState;
   }
 }
 
 class Table {
   constructor(x, y, cadena) {
-    this.matriz = this.rellenarMatriz(x, y, cadena);
+      this.matriz = this.rellenarMatriz(x, y, cadena);
   }
 
   generarMatriz(x, y) {
-    return Array(x).fill(new Cell()).map(item => (new Array(y).fill(new Cell())));
+      return Array(x).fill(new Cell()).map(item => (new Array(y).fill(new Cell())));
   }
 
   rellenarMatriz(x, y, cadena) {
-    let matriz = this.generarMatriz(x, y);
-    matriz.forEach(async (fila, indexi) => {
-      fila.forEach(async (casilla, indexj) => {
-        matriz[indexi][indexj] = new Cell(cadena.charAt(this.randomCaracter(0, cadena.length - 1)))
+      let matriz = this.generarMatriz(x, y);
+      matriz.forEach(async (fila, indexi) => {
+          fila.forEach(async (casilla, indexj) => {
+              matriz[indexi][indexj] = new Cell(cadena.charAt(this.randomCaracter(0, cadena.length - 1)))
+          })
       })
-    })
-    return matriz;
+      return matriz;
   }
 
   randomCaracter(min, max) {
-    return Math.random() * (max - min) + min;
+      return Math.random() * (max - min) + min;
   }
 
   cicloVida() {
-    return "";
+      return this.reglas();
   }
 
   /*
@@ -45,91 +45,102 @@ class Table {
   */
 
   vecinosVivos(x, y) {
-    var vecinosVivos = 0;
-    const objeto = {
-      esquinaSuperiorIzquierda: (fila, columna) => {
-        if (fila != 0) {
-          if (this.matriz[fila - 1][columna - 1].getState() == "*") {
-            vecinosVivos++;
-          }
-        }
-      },
-      superior: (fila, columna) => {
-        if (fila != 0) {
-          if (this.matriz[fila - 1][columna].getState() == "*") {
-            vecinosVivos++;
-          }
-        }
-      },
-      esquinaSuperiorDerecha: (fila, columna) => {
-        if (fila != 0) {
-          if (this.matriz[fila - 1][columna + 1].getState() == "*") {
-            vecinosVivos++;
-          }
-        }
-      },
-      esquinaInferiorIzquierda: (fila, columna) => {
-        if (fila != this.matriz.length - 1) {
-          if (this.matriz[fila + 1][columna - 1].getState() == "*") {
-            vecinosVivos++;
-          }
-        }
-      },
-      inferior: (fila, columna) => {
-        if (fila != this.matriz.length - 1) {
-          if (this.matriz[fila + 1][columna].getState() == "*") {
-            vecinosVivos++;
-          }
-        }
-      },
-      esquinaInferiorDerecha: (fila, columna) => {
-        if (fila != this.matriz.length - 1) {
-          if (this.matriz[fila + 1][columna + 1].getState() == "*") {
-            vecinosVivos++;
-          }
-        }
-      },
-      derecha: (fila, columna) => {
-        if (this.matriz[fila][columna + 1].getState() == "*") {
-          vecinosVivos++;
-        }
-      },
-      izquierda: (fila, columna) => {
-        if (this.matriz[fila][columna - 1].getState() == "*") {
-          vecinosVivos++;
-        }
-      },
-    }
-    Object.keys(objeto).forEach(key => objeto[key](x, y))
-    return vecinosVivos;
+      var vecinosVivos = 0;
+      const objeto = {
+          esquinaSuperiorIzquierda: (fila, columna) => {
+              if (fila != 0 && this.matriz[fila - 1][columna - 1]) {
+                  if (this.matriz[fila - 1][columna - 1].getState() == "*") {
+                      vecinosVivos++;
+                  }
+              }
+          },
+          superior: (fila, columna) => {
+              if (fila != 0 && this.matriz[fila - 1][columna]) {
+                  if (this.matriz[fila - 1][columna].getState() == "*") {
+                      vecinosVivos++;
+                  }
+              }
+          },
+          esquinaSuperiorDerecha: (fila, columna) => {
+              if (fila != 0 && this.matriz[fila - 1][columna + 1]) {
+                  if (this.matriz[fila - 1][columna + 1].getState() == "*") {
+                      vecinosVivos++;
+                  }
+              }
+          },
+          esquinaInferiorIzquierda: (fila, columna) => {
+              if (fila != this.matriz.length - 1 && this.matriz[fila + 1][columna - 1]) {
+                  if (this.matriz[fila + 1][columna - 1].getState() == "*") {
+                      vecinosVivos++;
+                  }
+              }
+          },
+          inferior: (fila, columna) => {
+              if (fila != this.matriz.length - 1 && this.matriz[fila + 1][columna]) {
+                  if (this.matriz[fila + 1][columna].getState() == "*") {
+                      vecinosVivos++;
+                  }
+              }
+          },
+          esquinaInferiorDerecha: (fila, columna) => {
+              if (fila != this.matriz.length - 1 && this.matriz[fila + 1][columna + 1]) {
+                  if (this.matriz[fila + 1][columna + 1].getState() == "*") {
+                      vecinosVivos++;
+                  }
+              }
+          },
+          derecha: (fila, columna) => {
+              if (this.matriz[fila][columna + 1]) {
+                  if (this.matriz[fila][columna + 1].getState() == "*") {
+                      vecinosVivos++;
+                  }
+              }
+          },
+          izquierda: (fila, columna) => {
+              if (this.matriz[fila][columna - 1]) {
+                  if (this.matriz[fila][columna - 1].getState() == "*") {
+                      vecinosVivos++;
+                  }
+              }
+          },
+      }
+      Object.keys(objeto).forEach(key => objeto[key](x, y))
+      return vecinosVivos;
   }
 
   imprimirMatriz(fila = 0) {
-    var cadena = "";
-    if (fila == this.matriz.length) return "\n";
-    this.matriz[fila].forEach(obj => {
-      cadena = cadena + obj.getState();
-    })
-    return cadena + "\n" + this.imprimirMatriz(fila + 1);
+      var cadena = "";
+      if (fila == this.matriz.length) return "\n";
+      this.matriz[fila].forEach(obj => {
+          cadena = cadena + obj.getState();
+      })
+      return cadena + "\n" + this.imprimirMatriz(fila + 1);
   }
 
   reglas() {
-    for (let i = 0; i < this.matriz.length(); i++) {
-      for (let j = 0; j <= this.matriz.length(); j++) {
-        vivas = vecinosVivos(i, j);
-        if (array[i][j].getState() == '*' && vivas < 2) {
-          this.matriz[i][j].setState() = '.';
-        } else if (array[i][j].getState() == '*' && vivas > 3) {
-          this.matriz[i][j].setState() = '.';
-        } else if (array[i][j].getState() == '.' && vivas == 3) {
-          this.matriz[i][j].setState() = '*';
-        }
+      for (let i = 0; i < this.matriz.length; i++) {
+          for (let j = 0; j < this.matriz.length; j++) {
+              let vivas = this.vecinosVivos(i, j);
+              //    console.log(this.matriz[i][j].status);
+              if (this.matriz[i][j].getState() == '*' && vivas < 2) {
+                  this.matriz[i][j].setState('.');
+              } else if (this.matriz[i][j].getState() == '*' && vivas > 3) {
+                  this.matriz[i][j].setState('.');
+              } else if (this.matriz[i][j].getState() == '.' && vivas == 3) {
+                  this.matriz[i][j].setState('*');
+              }
+
+          }
       }
-    }
-    return this.matriz;
+      return this.matriz;
   }
 }
 
 var tabla = new Table(10, 10, ".......*....**....**.");
 console.log(tabla.imprimirMatriz(0));
-console.log(tabla.vecinosVivos(3, 3));
+//console.log(tabla.vecinosVivos(3, 3));
+
+setInterval(() => {
+  tabla.cicloVida();
+  console.log(tabla.imprimirMatriz(0));
+}, 2000);
